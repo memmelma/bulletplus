10ee7c4eb3098d83ce4013c370b92c73e552fce4
memmelma <marius.memmel@gmx.net>
2018-10-29 16:31:39 -0400

    setup layout and menu

---
+++ app/src/main/java/com/mmr/marius/bulletplus/Note.java
@@ -0,0 +1,30 @@
+package com.mmr.marius.bulletplus;
+
+public class Note {
+
+    private String title;
+    private String description;
+    private int priority;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public Note(){
+        //empty constructor needed for FireBase
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
